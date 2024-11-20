b430b205333c1a0a5002efd09e6d9ccb9be5cff2
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2024-11-20 09:30:33 +0000

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Andrea Telatin (QIB) on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RollDiceTests/RollDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceTests.swift
+//  RollDiceTests
+//
+//  Created by Andrea Telatin (QIB) on 20/11/2024.
+//
+
+import Testing
+@testable import RollDice
+
+struct RollDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
